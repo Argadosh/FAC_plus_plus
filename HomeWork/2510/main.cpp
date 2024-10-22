f6cda8ddd1005e9408d8ad0bdc6d2099a545356a
Argadosh <lbvf5550@mail.ru>
2024-10-23 01:52:00 +0500

    Update HW/2510

--- HomeWork/2510/main.cpp
+++ HomeWork/2510/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <random>
+#include <chrono> //for 3.3
 
 
 //for 1
@@ -141,8 +143,130 @@ void quick_sort_stack(stack_t& src) {
 }
 
 
+//for 3.2
+template <typename stack_t>
+void reverse_stack(stack_t& src) {
+	stack_t tmp;
+	while (src.size() != 0) {
+		tmp.push(src.top());
+		src.pop();
+	}
+	src = tmp;
+}
+
+//task 3.2 Quick Sort with 2 support elenemts
+template <typename stack_t>
+void quick_sort_stack_two(stack_t& src) {
+
+	if (src.size() <= 1) {
+		return;
+	}
+
+	auto s_el_1 = src.top();
+	src.pop();
+	auto s_el_2 = src.top();
+	src.pop();
+
+	if (s_el_1 > s_el_2) {
+		auto tmp = s_el_2;
+		s_el_2 = s_el_1;
+		s_el_1 = tmp;
+	}
+
+	stack_t great, less, between;
+
+	while (src.size() != 0) {
+		auto cur_el = src.top();
+		src.pop();
+
+		if (cur_el < s_el_1) {
+			less.push(cur_el);
+		}
+		else if (cur_el > s_el_2) {
+			great.push(cur_el);
+		}
+		else {
+			between.push(cur_el);
+		}
+	}
+
+	quick_sort_stack_two(great);
+	quick_sort_stack_two(less);
+	quick_sort_stack_two(between);
+
+	reverse_stack(great);
+	reverse_stack(between);
+	reverse_stack(less);
+
+	while (great.size() != 0) {
+		src.push(great.top());
+		great.pop();
+	}
+
+	src.push(s_el_2);
+
+	while (between.size() != 0) {
+		src.push(between.top());
+		between.pop();
+	}
+
+	src.push(s_el_1);
+
+	while (less.size() != 0) {
+		src.push(less.top());
+		less.pop();
+	}
+
+}
 
 int main() {
+
+	std::stack<int> s;
+
+	std::mt19937 engine;
+	engine.seed(std::time(nullptr));
+
+	for (size_t i = 0; i < 100; i++) {
+		int val = engine();
+		s.push(val);
+	}
+
+	std::stack<int> tmp = s;
+
+	while (tmp.size() != 0) {
+		std::cout << tmp.top() << std::endl;
+		tmp.pop();
+	}
+
+	tmp = s;
+
+	std::cout << "\n\n\n" << std::endl;
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	quick_sort_stack(tmp);
+
+	auto end = std::chrono::high_resolution_clock::now();
+
+	auto res_1 = end - start;
+
+	start = std::chrono::high_resolution_clock::now();
+
+	quick_sort_stack_two(s);
+
+	end = std::chrono::high_resolution_clock::now();
+
+	auto res_2 = end - start;
+
+	while (s.size() != 0) {
+		std::cout << s.top() << std::endl;
+		s.pop();
+	}
+
+	std::cout << "\n\n\n" << std::endl;
+
+	std::cout << "Time first method: " << res_1.count() << std::endl;
+	std::cout << "Time second method: " << res_2.count() << std::endl;
 
 	return 0;
 }
